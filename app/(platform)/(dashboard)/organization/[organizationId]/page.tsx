f9eb08adbd0ae219a86911915742bc5ee3d8ea6c
Maaz-Ahmed007 <captainzds007@gmail.com>
2024-01-02 00:24:05 +0500

    update 3

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,16 @@
+import { Separator } from "@/components/ui/separator"
+
+import { Info } from "./_components/info"
+import { BoardList } from "./_components/board-list"
+
 const OrganizationIdPage = () => {
     return (
-        <div>
-            <form>
-                <input
-                    id="title"
-                    name="title"
-                    required
-                />
-            </form>
+        <div className="w-full mb-20">
+            <Info />
+            <Separator className="my-4" />
+            <div className="px-2 md:px-4">
+                <BoardList />
+            </div>
         </div>
     )
 }
